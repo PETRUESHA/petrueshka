8e800389a2d24ec9404c771712e1e6327840b633
Jakalgit <you@example.com>
2022-02-17 18:35:22 +0300

    comment

--- src/main/java/com/example/petrueshka/MyController.java
+++ src/main/java/com/example/petrueshka/MyController.java
@@ -3,6 +3,8 @@ package com.example.petrueshka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+// comment
 @Controller
 public class MyController {
 
